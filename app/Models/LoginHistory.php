907d4d2ea58229494be71fc5127ca30996f517fc
Kyle Salopek <krs1998@gmail.com>
2026-06-19 11:35:28 -0500

    Fix IP address not saving and displaying in activity history

--- app/Models/LoginHistory.php
+++ app/Models/LoginHistory.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class LoginHistory extends Model
 {
-    protected $fillable = ['user_id'];
+    protected $fillable = ['user_id', 'ip_address'];
 
     public function user()
     {
